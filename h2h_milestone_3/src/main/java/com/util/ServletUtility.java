b9d728df15476d276158ef86a92127fad0ebf5ba
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-06 21:58:18 +0530

    Add files via upload

--- h2h_milestone_3/src/main/java/com/util/ServletUtility.java
+++ h2h_milestone_3/src/main/java/com/util/ServletUtility.java
@@ -1,1 +1,13 @@
+package com.highradius.util;
 
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletUtility 
+{
+
+	public static void redirect(HttpServletResponse response, String string) 
+	{
+		
+	}
+
+}
