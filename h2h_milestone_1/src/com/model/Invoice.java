7dda8f31cc5bf4c1341260a0edb0fa57cdfab736
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-06 21:00:23 +0530

    Add files via upload

--- h2h_milestone_1/src/com/model/Invoice.java
+++ h2h_milestone_1/src/com/model/Invoice.java
@@ -1,1 +1,50 @@
+/*Package com.highradius.model contains 1 file(s) 
+a. Invoice 
+   i. It is a class 
+   ii. Parameterized Constructor 
+   iii. Method getter(s) and setter(s)
+*/
+
+package com.highradius.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Invoice 
+{
+	private int id;
+
+    public Invoice(int id) 
+    {
+        this.id = id;
+    }
+
+    public int getInvoiceNumber() 
+    {
+        return id;
+    }
+
+    public void setInvoiceNumber(int id) 
+    {
+        this.id = id;
+    }
+
+    // Example of using ArrayList to store multiple instances of Invoice
+    public static void main(String[] args) 
+    {
+        List<Invoice> invoiceList = new ArrayList<>();
+
+        Invoice invoice1 = new Invoice(1);
+        invoiceList.add(invoice1);
+
+        Invoice invoice2 = new Invoice(2);
+        invoiceList.add(invoice2);
+
+        for (Invoice invoice : invoiceList) 
+        {
+            System.out.println("Invoice Number: " + invoice.getInvoiceNumber());
+            System.out.println();
+        }
+    }
+}
 
