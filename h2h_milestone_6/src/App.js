494eded3c4520a4fb44984f907143809605a7389
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-06 20:49:55 +0530

    Add files via upload

---
+++ h2h_milestone_6/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import DataTable from './components/DataTable';
+// import InvoiceTable from './components/InvoiceTable';
+
+function App() {
+  return (
+    <div className='App'>
+      <DataTable/>
+      {/* <InvoiceTable /> */}
+    </div>
+
+  );
+}
+
+export default App;
