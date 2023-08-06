b4f4ae20cf89551f4d070cb1ba6d3ae5f673f168
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-06 21:08:17 +0530

    Add files via upload

--- h2h_milestone_2/src/com/connection/DatabaseConnector.java
+++ h2h_milestone_2/src/com/connection/DatabaseConnector.java
@@ -1,1 +1,35 @@
+package com.highradius.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/milestone_two";
+        String username = "root";
+        String password = "Thisarakh2h11126k@";
+
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+
+            try (Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery("SELECT AMOUNT_IN_USD FROM h2h_oap")) {
+            	int count = 0;
+
+                while (rs.next()) {
+                    float amount = rs.getFloat("AMOUNT_IN_USD");
+                    System.out.println(amount);
+                    count++;
+                 }
+                System.out.println("count data:" + count);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
