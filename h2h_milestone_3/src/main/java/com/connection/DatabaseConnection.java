7c44aeccf2429133c32d6920e5440a3d2cdc887a
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-06 21:54:39 +0530

    Add files via upload

--- h2h_milestone_3/src/main/java/com/connection/DatabaseConnection.java
+++ h2h_milestone_3/src/main/java/com/connection/DatabaseConnection.java
@@ -1,1 +1,18 @@
+package com.highradius.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection 
+{
+	private static final String url= "jdbc:mysql://localhost:3306/milestone_two";
+	private static final String username = "root";
+	private static final String password= "Thisarakh2h11126k@";
+
+	public static Connection getConnection() throws SQLException 
+	{
+	        return DriverManager.getConnection(url, username, password);
+	}
+}
 
