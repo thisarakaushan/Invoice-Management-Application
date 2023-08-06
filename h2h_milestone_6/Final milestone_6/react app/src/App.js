fd55acf9b5db5a8368450296546f42135266e7d5
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-06 20:51:48 +0530

    Add files via upload

--- h2h_milestone_6/Final milestone_6/react app/src/App.js
+++ h2h_milestone_6/Final milestone_6/react app/src/App.js
@@ -1,1 +1,34 @@
+import React from 'react';
+import Table from './components/Table';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My App</h1>
+      <table />
+    </div>
+  );
+};
+
+export default App;
+
+
+// import React from 'react';
+// import './App.css';
+// import DataTable from './components/DataTable';
+// // import InvoiceTable from './components/InvoiceTable';
+// import 
+
+// function App() {
+//   return (
+//     <div className='App'>
+//       <Table/>
+//       {/* <InvoiceTable /> */}
+//     </div>
+
+//   );
+// }
+
+// export default App;
+
 
---
+++ h2h_milestone_6/Final milestone_6/react app/src/theme.js
@@ -0,0 +1,30 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#505050",
+    },
+    secondary: {
+      main: "#283d4a",
+    },
+    outline: {
+      main: "#fff",
+    },
+    text: {
+      main: "#fff",
+    },
+    link: {
+      main: "#0d6efd",
+    },
+    focusBlue: {
+      main: "#14aff1",
+    },
+    searchInputText: {
+      main: "#616161",
+    },
+    background: {
+        default: "#2d4250", // Set your desired background color here
+      },
+  },
+});
